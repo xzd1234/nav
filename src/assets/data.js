3d3b9ba5b7c62bc33f366624f0d8e804f6e81caa
xzd <xie_zhendong@163.com>
2022-03-30 15:36:59 +0800

    修改vue3地址

--- src/assets/data.js
+++ src/assets/data.js
@@ -19,7 +19,7 @@ const data = {
           name: "Vue3",
           desc: "构建数据驱动的web渐进式框架",
           icon: "https://xiezhendong.cn/static/images/vue.png",
-          url: "https://v3.cn.vuejs.org/"
+          url: "https://staging-cn.vuejs.org/"
         },
         {
           name: "jQuery",
